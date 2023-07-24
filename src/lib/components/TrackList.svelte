eb7dac5d46a49ce61449c05b76242dda7a4d8bc6
Acha0203 <74553433+Acha0203@users.noreply.github.com>
2023-07-24 14:36:59 +0900

    feat: create an audio player component

--- src/lib/components/TrackList.svelte
+++ src/lib/components/TrackList.svelte
@@ -1,6 +1,7 @@
 <script lang="ts">
   import { msToTime } from '$helpers';
   import { Clock8, ListPlus } from 'lucide-svelte';
+  import Player from './Player.svelte';
 
   export let tracks:
     | SpotifyApi.TrackObjectFull[]
@@ -24,6 +25,17 @@
     <div class="row">
       <div class="number-column">
         <span class="number">{index + 1}</span>
+        <div class="player">
+          <Player
+            {track}
+            on:play={(e) => {
+              console.log(e.detail.track);
+            }}
+            on:pause={(e) => {
+              console.log(e.detail.track);
+            }}
+          />
+        </div>
       </div>
       <div class="info-column">
         <div class="track-title">
@@ -89,7 +101,7 @@
       .info-column {
         flex: 1;
         .track-title {
-          display: flex; 
+          display: flex;
           align-items: center;
           h4 {
             margin: 0;
