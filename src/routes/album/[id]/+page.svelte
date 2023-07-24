e9a4892cee6805834dbfb1b1cea67ccadc74e269
Acha0203 <74553433+Acha0203@users.noreply.github.com>
2023-07-24 10:25:42 +0900

    feat: use the average color endpoint in the album page

--- src/routes/album/[id]/+page.svelte
+++ src/routes/album/[id]/+page.svelte
@@ -1,18 +1,18 @@
 <script lang="ts">
-  import { getCopyrightSymbol } from '$helpers';
+  // import { getCopyrightSymbol } from '$helpers';
   import { ItemPage } from '$components';
   import type { PageData } from './$types';
 
   export let data: PageData;
 
   $: album = data.album;
-  $: console.log(album);
+  $: color = data.color;
 </script>
 
 <ItemPage
   title={album.name}
   type={album.album_type}
-  color={'orange'}
+  {color}
   image={album.images.length > 0 ? album.images[0].url : undefined}
 >
   <p class="meta" slot="meta">
