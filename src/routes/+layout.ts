43c862872a6cd4444ef4de76ef4f5efc2ade2170
Acha0203 <74553433+Acha0203@users.noreply.github.com>
2023-07-28 22:44:37 +0900

    chore: modify deploy.yml

--- src/routes/+layout.ts
+++ src/routes/+layout.ts
@@ -1,6 +1,8 @@
 import { redirect } from '@sveltejs/kit';
 import type { LayoutLoad } from './$types';
 
+export const prerender = true;
+
 export const load: LayoutLoad = ({ data, url }) => {
   const { user, userAllPlaylists } = data || {};
   if (user && url.pathname === '/login') {
--- svelte.config.js
+++ svelte.config.js
@@ -1,5 +1,7 @@
-import adapter from '@sveltejs/adapter-auto';
+import adapter from '@sveltejs/adapter-static';
 import preprocess from 'svelte-preprocess';
+
+const dev = process.argv.includes('dev');
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
@@ -16,6 +18,9 @@ const config = {
     // If your environment is not supported or you settled on a specific environment, switch out the adapter.
     // See https://kit.svelte.dev/docs/adapters for more information about adapters.
     adapter: adapter(),
+    paths: {
+      base: dev ? '' : process.env.BASE_PATH,
+    },
     alias: {
       $components: 'src/lib/components',
       $assets: 'src/assets',
