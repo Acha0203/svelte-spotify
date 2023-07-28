89ac657131b1ec7470c780dd91f6122a1ee95597
Acha0203 <74553433+Acha0203@users.noreply.github.com>
2023-07-29 00:58:27 +0900

    fix: modify vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -2,5 +2,17 @@ import { sveltekit } from '@sveltejs/kit/vite';
 import { defineConfig } from 'vite';
 
 export default defineConfig({
-	plugins: [sveltekit()]
+  plugins: [sveltekit()],
+  define: {
+    'process.env.VITE_SPOTIFY_APP_CLIENT_ID': JSON.stringify(
+      process.env.VITE_SPOTIFY_APP_CLIENT_ID
+    ),
+    'process.env.VITE_SPOTIFY_APP_CLIENT_SECRET': JSON.stringify(
+      process.env.VITE_SPOTIFY_APP_CLIENT_SECRET
+    ),
+    'process.env.VITE_SPOTIFY_BASE_URL': JSON.stringify(
+      process.env.VITE_SPOTIFY_BASE_URL
+    ),
+    'process.env.VITE_BASE_URL': JSON.stringify(process.env.VITE_BASE_URL),
+  },
 });
